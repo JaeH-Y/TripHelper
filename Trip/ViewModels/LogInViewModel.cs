08f5f3ae8e0ed0275c565958d0bfa3a946d35541
JaeH-Y <yjh21175753@gmail.com>
2025-11-19 08:39:16 +0900

    로그인 뷰모델 골조 구현 -> 상세 로직은 ServerPostService 생성해야할듯

---
+++ Trip/ViewModels/LogInViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Trip.ViewModels
+{
+    public class LogInViewModel : ViewModelBase
+    {
+
+        private string _idText;
+        public string IDText
+        {
+            get => _idText;
+            set => SetProperty(ref _idText, value);
+        }
+
+        private string _pwText;
+        public string PWText
+        {
+            get => _pwText;
+            set => SetProperty(ref _pwText, value);
+        }
+        public ICommand LogInCommand { get; set; }
+        public ICommand FindAccountCommand { get; set; }
+        public LogInViewModel()
+        {
+            CommandInit();
+        }
+        private void CommandInit()
+        {
+            LogInCommand = new AsyncRelayCommand(RequestLogIn);
+            FindAccountCommand = new AsyncRelayCommand(RequestFindAccount);
+        }
+
+        private async Task RequestLogIn()
+        {
+
+        }
+        private async Task RequestFindAccount()
+        {
+
+        }
+
+
+
+
+    }
+}
